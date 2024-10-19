9937e43f525aaa0efdb6b124b27adaa10855284b
devanaclimgo <anaclimgo@gmail.com>
2024-10-19 16:30:46 -0300

    dia 02

---
+++ dia-02/script.js
@@ -0,0 +1,9 @@
+function perguntas () {
+  const pessoa = {
+    nome: "Ana",
+    idade: 21,
+    linguagem: "Javascript"
+  }
+  console.log(`Olá ${pessoa.nome}, você tem ${pessoa.idade} anos e já está aprendendo ${pessoa.linguagem}!`);
+}
+perguntas()

e2e30729ce3fcd335f9b0510846e22396d824b85
devanaclimgo <anaclimgo@gmail.com>
2024-10-19 17:24:09 -0300

    solução dia 03

--- dia-03/script.js
+++ dia-03/script.js
@@ -1,26 +1,28 @@
-const area = prompt("Você deseja seguir para a área de Front-End ou seguir para a área de Back-End?");
+const area = prompt("Você deseja seguir para a área de Front-End ou seguir para a área de Back-End? Digite o nome da área :)");
+let linguagem = "";
 
-const respostaArea = prompt(area);
-
-if (respostaArea === "Front-End") {
-  console.log(prompt("Quer aprender React ou Vue?"))
-} else if (respostaArea === "Back-End") {
-  console.log(prompt("Quer aprender C# ou Java?"))
-}
-
-const seguir = prompt("Deseja seguir se especializando na área escolhida ou seguir se desenvolvendo para se tornar Fullstack?")
-
-const respostaSeguir = prompt(seguir)
-
-if (respostaSeguir == "Fullstack") {
-  console.log("Continue estudando que você chega lá!")
+if (area === "Front-End") {
+  linguagem = prompt("Quer aprender React ou Vue?")
+} else if (area === "Back-End") {
+  linguagem = prompt("Quer aprender C# ou Java?")
 } else {
-  console.log("Muito bem! Uma especialização sempre é importante")
+  alert ("Você não inseriu uma área válida!")
 }
 
-const tecnologias = prompt("Quais são as tecnologias que deseja se especializar?")
-const respostaTecnologias = prompt(tecnologias)
+const seguir = prompt("Digite 1 para seguir se especializando na área escolhida ou 2 para seguir se desenvolvendo para se tornar Fullstack")
 
-while (respostaTecnologias === "ok") {
-  console.log("Tem mais alguma tecnologia que deseja aprender?")
+if (seguir == 1) {
+  alert(`Continue se especializando em ${linguagem} para dominar a área de ${area}!`)
+} else if (seguir == 2) {
+  alert(`Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tornar Fullstack!`);
+} else {
+  alert("Você não inseriu um valor válido!")
+}
+
+let msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.")
+
+while (msg === "ok") {
+  let novaTecnologia = prompt("Qual?");
+  alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`)
+  msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.");
 }

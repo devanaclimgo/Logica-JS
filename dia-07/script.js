3b8631154c056d285d2b5589c1edcc03824fafa2
devanaclimgo <anaclimgo@gmail.com>
2024-10-21 18:35:05 -0300

    escopo dia 7

--- dia-07/script.js
+++ dia-07/script.js
@@ -5,3 +5,13 @@ const escolha = {
   divisao: [],
   sair: []
 }
+
+let continuar = true
+
+while (continuar) {
+  const escolher = prompt(`Escolha uma operação entre soma, subtração, multiplicação, divisão e sair`)
+
+  if(escolha.hasOwnProperty(escolher)){
+    
+  }
+}

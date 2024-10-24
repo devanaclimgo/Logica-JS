163c15a173b651ccfae1eeb1cdb44c7a41f4269f
devanaclimgo <anaclimgo@gmail.com>
2024-10-24 17:31:29 -0300

    escopo if escolha da operação

--- dia-07/script.js
+++ dia-07/script.js
@@ -12,6 +12,19 @@ while (continuar) {
   const escolher = prompt(`Escolha uma operação entre soma, subtração, multiplicação, divisão e sair`)
 
   if(escolha.hasOwnProperty(escolher)){
-    
+    escolher = escolha.soma
+    soma()
+  } else if (escolha.hasOwnProperty(escolher)) {
+    escolher = escolha.subtracao
+    subtracao()
+  } else if (escolha.hasOwnProperty(escolher)) {
+    escolher = escolha.multiplicacao
+    multiplicacao()
+  } else if (escolha.hasOwnProperty(escolher)) {
+    escolher = escolha.divisao
+    divisao()
+  } else if (escolha.hasOwnProperty(escolher)) {
+    escolher = escolha.sair
+    return
   }
 }

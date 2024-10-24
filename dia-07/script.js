561a37173c2d7bef9385e96e14d3a1305dae3d90
devanaclimgo <anaclimgo@gmail.com>
2024-10-24 18:26:03 -0300

    fazendo o sair funcionar

--- dia-07/script.js
+++ dia-07/script.js
@@ -15,16 +15,17 @@ let continuar = true
 while (continuar) {
   const escolher = prompt(`Escolha uma operação entre soma, subtração, multiplicação, divisão e sair`)
 
-  if(escolha.hasOwnProperty(escolher)){
+  if(escolha.hasOwnProperty(escolher) && escolher != 'sair'){
     let num1 = parseFloat(prompt("Digite o primeiro número:"))
     let num2 = parseFloat(prompt("Digite o segundo número:"))
     let resultado 
     = escolha[escolher](num1, num2)
     alert(`O resultado da ${escolher} é ${resultado}`)
-  } else if ( escolha === 'sair') {
-    alert('Até a próxima!')
-    break
+  } else if(escolher == 'sair') {
+    continuar = false
   } else {
-    alert('Escolha uma operação válida.')
+    alert('Escolha uma operação válida.');
   }
 }
+
+alert('Até a próxima!')

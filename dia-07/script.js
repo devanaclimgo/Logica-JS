1c24ebd2b05dc9e167b9424b14aa44e4972874c9
devanaclimgo <anaclimgo@gmail.com>
2024-10-24 17:52:39 -0300

    escopo simplificado + funções de cada operação

--- dia-07/script.js
+++ dia-07/script.js
@@ -1,9 +1,12 @@
 const escolha = {
-  soma: [],
-  subtracao: [],
-  multiplicacao: [],
-  divisao: [],
-  sair: []
+  soma: (a, b) => a + b,
+  subtracao: (a, b) => a - b,
+  multiplicacao: (a, b) => a * b,
+  divisao: (a, b) => {
+    if ( b === 0 ){
+      return "Divisão por zero não é permitido"
+    } return a / b
+  },
 }
 
 let continuar = true
@@ -12,19 +15,14 @@ while (continuar) {
   const escolher = prompt(`Escolha uma operação entre soma, subtração, multiplicação, divisão e sair`)
 
   if(escolha.hasOwnProperty(escolher)){
-    escolher = escolha.soma
-    soma()
-  } else if (escolha.hasOwnProperty(escolher)) {
-    escolher = escolha.subtracao
-    subtracao()
-  } else if (escolha.hasOwnProperty(escolher)) {
-    escolher = escolha.multiplicacao
-    multiplicacao()
-  } else if (escolha.hasOwnProperty(escolher)) {
-    escolher = escolha.divisao
-    divisao()
-  } else if (escolha.hasOwnProperty(escolher)) {
-    escolher = escolha.sair
-    return
+    let num1 = parseFloat(prompt("Digite o primeiro número:"))
+    let num2 = parseFloat(prompt("Digite o segundo número:"))
+    let resultado = escolha[escolher](num1, num2)
+    alert(`Resultado: ${resultado}`)
+  } else if ( escolha === 'sair') {
+    alert('Até a próxima!')
+    continuar = false
+  } else {
+    alert('Escolha uma operação válida.')
   }
 }
